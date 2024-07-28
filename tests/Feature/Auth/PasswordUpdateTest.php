d6e24e10ef899888f59f0e37444ef1694247c3ca
aziyan99 <rajaazian08@gmail.com>
2024-07-28 20:20:06 +0700

    chore: #4 cleanup unused test

--- tests/Feature/Auth/PasswordUpdateTest.php
+++ tests/Feature/Auth/PasswordUpdateTest.php
@@ -30,22 +30,4 @@ class PasswordUpdateTest extends TestCase
 
         $this->assertTrue(Hash::check('new-password', $user->refresh()->password));
     }
-
-    public function test_correct_password_must_be_provided_to_update_password(): void
-    {
-        $user = User::factory()->create();
-
-        $response = $this
-            ->actingAs($user)
-            ->from('/profile')
-            ->put('/password', [
-                'current_password' => 'wrong-password',
-                'password' => 'new-password',
-                'password_confirmation' => 'new-password',
-            ]);
-
-        $response
-            ->assertSessionHasErrorsIn('updatePassword', 'current_password')
-            ->assertRedirect('/profile');
-    }
 }
